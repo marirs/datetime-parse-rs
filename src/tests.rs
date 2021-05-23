c45e10ae7f767fac820f4482b5a9460f83524166
Sriram Govindan <marirs@gmail.com>
2021-05-23 20:25:40 +0530

    tests fix

--- src/tests.rs
+++ src/tests.rs
@@ -6,7 +6,7 @@ fn test_dotted_date() {
     let date = "1970.12.31";
     let test = date.parse::<DateTimeFixedOffset>();
     assert!(test.is_ok());
-    assert_eq!(test.unwrap().0.to_rfc3339(), "1970-12-31T00:00:00+05:30");
+    assert!(test.unwrap().0.to_rfc3339().starts_with("1970-12-31T00:00:00"));
 }
 
 #[test]
@@ -14,5 +14,5 @@ fn test_slash_date() {
     let date = "1970/12/31";
     let test = date.parse::<DateTimeFixedOffset>();
     assert!(test.is_ok());
-    assert_eq!(test.unwrap().0.to_rfc3339(), "1970-12-31T00:00:00+05:30");
+    assert!(test.unwrap().0.to_rfc3339().starts_with("1970-12-31T00:00:00+05:30"));
 }
