adf21aeecef24aa7e3952f938a7d04eae43e53c1
Sriram Govindan <marirs@gmail.com>
2021-05-23 21:16:59 +0530

    cargo fmt

--- src/tests.rs
+++ src/tests.rs
@@ -6,7 +6,11 @@ fn test_dotted_date() {
     let date = "1970.12.31";
     let test = date.parse::<DateTimeFixedOffset>();
     assert!(test.is_ok());
-    assert!(test.unwrap().0.to_rfc3339().starts_with("1970-12-31T00:00:00"));
+    assert!(test
+        .unwrap()
+        .0
+        .to_rfc3339()
+        .starts_with("1970-12-31T00:00:00"));
 }
 
 #[test]
@@ -14,5 +18,9 @@ fn test_slash_date() {
     let date = "1970/12/31";
     let test = date.parse::<DateTimeFixedOffset>();
     assert!(test.is_ok());
-    assert!(test.unwrap().0.to_rfc3339().starts_with("1970-12-31T00:00:00"));
+    assert!(test
+        .unwrap()
+        .0
+        .to_rfc3339()
+        .starts_with("1970-12-31T00:00:00"));
 }
