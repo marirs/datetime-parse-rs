8558f6186cf31ac7a59ed7a98e18c52b1605935e
marirs <marirs@gmail.com>
2022-02-20 13:03:31 +0530

    more checks

--- src/lib.rs
+++ src/lib.rs
@@ -193,7 +193,12 @@ fn try_dmmmy_hms_tz(s: &str) -> Result<DateTime<FixedOffset>, Error> {
 // Wed Jul 1 1970 13:13:55 GMT+0000
 fn try_mmmddyyyy_hms_tz(s: &str) -> Result<DateTime<FixedOffset>, Error> {
     let tz = s.rsplitn(2, ' ').take(2).collect::<Vec<_>>()[0].replace("GMT", "");
-    let dt = s.rsplitn(2, ' ').take(2).collect::<Vec<_>>()[1];
+    let dt;
+    if s.rsplitn(2, ' ').count() > 1 {
+        dt = s.rsplitn(2, ' ').take(2).collect::<Vec<_>>()[1].to_string();
+    } else {
+        return Err("custom parsing failed".to_string());
+    };
     if !tz.is_empty() {
         let x = dt.to_string() + " " + &tz.replace(':', "");
         DateTime::parse_from_str(&x, "%B %d %Y %H:%M:%S %z")
